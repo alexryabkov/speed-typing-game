752bcf2dc9ce0259be623fc38a1629352758b49c
Alexander Ryabkov <alexryabkov@gmail.com>
2020-05-03 16:59:42 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
 
 function App() {
+  const STARTING_TIME = 5
+
+  const [text, setText] = useState('')
+  const [wordCount, setWordCount] = useState(0)
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const textBoxRef = useRef(null)
+
+  const startGame = () => {
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText('')
+    textBoxRef.current.disabled = false
+    textBoxRef.current.focus()
+  }
+
+  useEffect(() => {
+    if(isTimeRunning && timeRemaining > 0) {
+        setTimeout(() => {
+            setTimeRemaining(time => time - 1)
+        }, 1000)
+    } else {
+        setIsTimeRunning(false)
+        setWordCount(text.split(' ').filter(w => w !== '').length)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [timeRemaining, isTimeRunning])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <h1>How fast do you type?</h1>
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            disabled={!isTimeRunning}
+            ref={textBoxRef}
+          />
+          <h4>Time remaining: {timeRemaining}</h4>
+          <button
+            onClick={startGame}
+            disabled={isTimeRunning}
+          >
+            Start
+          </button>
+          <h1>Word count: {wordCount}</h1>
+      </div>
+  )
 }
 
 export default App;
